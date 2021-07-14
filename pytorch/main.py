89e0f815779c3fc8c84fadc1d7dc6579c6be883d
xushanchuan <aisensiy@163.com>
2021-07-14 14:38:21 +0800

    fix: update pytorch config https://github.com/signcl/issues/issues/639

--- pytorch/main.py
+++ pytorch/main.py
@@ -4,13 +4,12 @@ from torch.autograd import Variable
 import torch.utils.data.dataloader as Data
 import openbayestool
 import argparse
-import os
 
-parser = argparse.ArgumentParser(description='hypertuning')
-parser.add_argument('--input', help='input')
-parser.add_argument('--filters', help='filters')
-parser.add_argument('--nn', help='nn')
-parser.add_argument('--opt', help='opt')
+parser = argparse.ArgumentParser(description="hypertuning")
+parser.add_argument("--input", help="input")
+parser.add_argument("--filters", help="filters")
+parser.add_argument("--nn", help="nn")
+parser.add_argument("--opt", help="opt")
 args = parser.parse_args()
 print(args.filters, args.nn, args.opt)
 
@@ -22,14 +21,17 @@ nb_filters = int(float(args.filters))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 train_data = torchvision.datasets.MNIST(
-   root = './mnist', train=True, transform=torchvision.transforms.ToTensor(), download=True
+    root=args.input,
+    train=True,
+    transform=torchvision.transforms.ToTensor(),
+    download=False,
 )
 test_data = torchvision.datasets.MNIST(
-    root = './mnist', train=False, transform=torchvision.transforms.ToTensor()
+    root=args.input, train=False, transform=torchvision.transforms.ToTensor()
 )
-print("train_data:", train_data.train_data.size())
-print("train_labels:", train_data.train_labels.size())
-print("test_data:", test_data.test_data.size())
+print("train_data:", train_data.data.size())
+print("train_labels:", train_data.targets.size())
+print("test_data:", test_data.data.size())
 
 train_loader = Data.DataLoader(dataset=train_data, batch_size=16, shuffle=True)
 test_loader = Data.DataLoader(dataset=test_data, batch_size=16)
@@ -41,21 +43,22 @@ class Net(torch.nn.Module):
         self.conv1 = torch.nn.Sequential(
             torch.nn.Conv2d(1, nb_filters, 3, 1, 1),
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2))
+            torch.nn.MaxPool2d(2),
+        )
         self.conv2 = torch.nn.Sequential(
-            torch.nn.Conv2d(nb_filters, nb_filters*2, 3, 1, 1),
+            torch.nn.Conv2d(nb_filters, nb_filters * 2, 3, 1, 1),
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)
+            torch.nn.MaxPool2d(2),
         )
         self.conv3 = torch.nn.Sequential(
-            torch.nn.Conv2d(nb_filters*2, nb_filters*2, 3, 1, 1),
+            torch.nn.Conv2d(nb_filters * 2, nb_filters * 2, 3, 1, 1),
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)
+            torch.nn.MaxPool2d(2),
         )
         self.dense = torch.nn.Sequential(
-            torch.nn.Linear(nb_filters*2 * 3 * 3, dense_num),
+            torch.nn.Linear(nb_filters * 2 * 3 * 3, dense_num),
             torch.nn.ReLU(),
-            torch.nn.Linear(dense_num, 10)
+            torch.nn.Linear(dense_num, 10),
         )
 
     def forward(self, x):
@@ -71,22 +74,23 @@ model = Net()
 model.to(device)
 print(model)
 
-if args.opt == 'Adam':
+if args.opt == "Adam":
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-elif args.opt == 'SGD':
+elif args.opt == "SGD":
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-elif args.opt == 'Adadelta':
-    optimizer = torch.optim.Adadelta(model.parameters(), lr=0.01, rho=0.9, eps=1e-06, weight_decay=0)
+elif args.opt == "Adadelta":
+    optimizer = torch.optim.Adadelta(
+        model.parameters(), lr=0.01, rho=0.9, eps=1e-06, weight_decay=0
+    )
 
-    
-    
+
 loss_func = torch.nn.CrossEntropyLoss()
 
 for epoch in range(10):
-    print('epoch {}'.format(epoch + 1))
+    print("epoch {}".format(epoch + 1))
     # training-----------------------------
-    train_loss = 0.
-    train_acc = 0.
+    train_loss = 0.0
+    train_acc = 0.0
     for batch_x, batch_y in train_loader:
         batch_x, batch_y = Variable(batch_x), Variable(batch_y)
         batch_x = batch_x.to(device)
@@ -100,15 +104,20 @@ for epoch in range(10):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    print('Train Loss: {:.6f}, Acc: {:.6f}'.format(train_loss / (len(
-        train_data)), train_acc / (len(train_data))))
+    print(
+        "Train Loss: {:.6f}, Acc: {:.6f}".format(
+            train_loss / (len(train_data)), train_acc / (len(train_data))
+        )
+    )
 
     # evaluation--------------------------------
     model.eval()
-    eval_loss = 0.
-    eval_acc = 0.
+    eval_loss = 0.0
+    eval_acc = 0.0
     for batch_x, batch_y in test_loader:
-        batch_x, batch_y = Variable(batch_x, volatile=True), Variable(batch_y, volatile=True)
+        batch_x, batch_y = Variable(batch_x, volatile=True), Variable(
+            batch_y, volatile=True
+        )
         batch_x = batch_x.to(device)
         batch_y = batch_y.to(device)
         out = model(batch_x)
@@ -117,6 +126,10 @@ for epoch in range(10):
         pred = torch.max(out, 1)[1]
         num_correct = (pred == batch_y).sum()
         eval_acc += num_correct.item()
-    print('Test Loss: {:.6f}, Acc: {:.6f}'.format(eval_loss / (len(test_data)), eval_acc / (len(test_data))))
-    openbayestool.log_metric('loss', eval_loss / (len(test_data)))
-    openbayestool.log_metric('acc', eval_acc / (len(test_data)))
+    print(
+        "Test Loss: {:.6f}, Acc: {:.6f}".format(
+            eval_loss / (len(test_data)), eval_acc / (len(test_data))
+        )
+    )
+    openbayestool.log_metric("loss", eval_loss / (len(test_data)))
+    openbayestool.log_metric("acc", eval_acc / (len(test_data)))
